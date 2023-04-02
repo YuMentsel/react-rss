521f2b3c070a5134756ba6790d7c0d629b76deaf
Yu Mentsel <yu.mentsel@gmail.com>
2023-04-03 00:09:39 +0300

    fix: fix error message

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -7,7 +7,7 @@ export enum ErrorsMessages {
   titleNotValid = 'Enter minimum 3 characters.',
   type = 'Choose a card type.',
   date = 'Choose a delivery date.',
-  dateNotValid = 'Select a date within next 5 days from tomorrow.',
+  dateNotValid = 'Select a date within next 6 days from tomorrow.',
   discount = 'Choose a discount size.',
   stock = 'Confirm stock status.',
   image = 'Add an image.',
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,8 @@
 export function dateValidation(date: string) {
   const currDate = new Date().getTime();
-  const UTCdelivery = new Date().getTimezoneOffset() * 60000;
-  const deliveryDate = new Date(date).getTime() + UTCdelivery;
-  const maxDeliveryDate = currDate + UTCdelivery + 518400000;
+  const UTC = new Date().getTimezoneOffset() * 60000;
+  const deliveryDate = new Date(date).getTime() + UTC;
+  const maxDeliveryDate = currDate + UTC + 518400000;
 
   return currDate < deliveryDate && deliveryDate < maxDeliveryDate;
 }
