a02dd7853dc085a0a94fb5a487c29b3da16f22cd
Yu Mentsel <yu.mentsel@gmail.com>
2023-04-03 03:36:16 +0300

    fix: fix date validation

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,7 @@ export function dateValidation(date: string) {
   const currDate = new Date().getTime();
   const UTC = new Date().getTimezoneOffset() * 60000;
   const deliveryDate = new Date(date).getTime() + UTC;
-  const maxDeliveryDate = currDate + UTC + 518400000;
+  const maxDeliveryDate = currDate + 518400000;
 
   return currDate < deliveryDate && deliveryDate < maxDeliveryDate;
 }
